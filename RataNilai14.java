7da75eae168685c4f90a4821699c819471bf4d28
adamarsyadfaizin <adamkian09@gmail.com>
2024-10-30 01:14:11 +0000

    CommitPercobaan4

---
+++ RataNilai14.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class RataNilai14 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i,j;
+        float nilai, totalNilai, rataNilai;
+        int nilaiMhs;
+        i = 1;
+        while (i<=5) {
+            totalNilai = 0;
+            for (j=1;j<=5;j++) {
+                System.out.print("Nilai ke-" +j + " = ");
+                nilaiMhs = sc.nextInt();
+                totalNilai+=nilaiMhs;
+            }
+            rataNilai = totalNilai/5;
+            i++;
+            System.out.println("Rata-rata Nilai Mahasiswa ke " + i + " adalah " + rataNilai);
+            System.out.println("Input Nilai Mahasiswa ke " + i);
+        }
+        
+    }
+}
